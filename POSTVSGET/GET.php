a7ff52ee9a8fb7fa610cc230a5766a56eb44eb84
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-23 16:00:12 +0000

    Update GET.php

--- POSTVSGET/GET.php
+++ POSTVSGET/GET.php
@@ -3,14 +3,14 @@
       <head>
       </head>
       <body>
-            <form action=”site.php” method= “get”>
-            Password:<input type=“password” name= “password”> <br>
-            <input type= “submit”>
+            <form action="site.php" method= "get">
+            Password:<input type="password" name= "password"> <br>
+            <input type= "submit">
             </form>
             <br></br>
 
              <?php
-	                echo $_GET[“password”];
+	                echo $_GET["password"];
              ?>
       </body>
 </html>
