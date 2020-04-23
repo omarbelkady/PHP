acd142c28bfd7e2bfb5b20b423b42e6a0e9d76e7
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-23 00:01:00 +0000

    Create Mysite.php

---
+++ Mysite.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset=”utf-8”>
+  <title></title>
+</head>
+<body>
+  <h1>Mike’s site</h1>
+    <?php
+        echo "<h1>Omar's site 4eva</h1>";
+        echo "<hr>";
+        echo "<p>This is my site</p>";
+    ?>
+</body>
+</html>
