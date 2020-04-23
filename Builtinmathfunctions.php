5a14112298a744dc9e06b1e41c70c12a1ce0b87d
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-23 15:46:16 +0000

    Create Builtinmathfunctions.php

---
+++ Builtinmathfunctions.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php
+	echo pow(2,4);
+	echo sqrt(144);//print 12
+	echo max(2, 10);//prints 10
+	echo min(2,10);//print 2
+	echo round(3.2);//prints 3
+	echo ceil(3.3);//prints 4
+	echo floor(3.9);//prints 3
+
+?>
+</body>
+</html>
