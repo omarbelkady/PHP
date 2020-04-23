0a35755cd2ac6dca61844e9dc82e631010bcdbbf
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-23 16:09:59 +0000

    Create userinputfromcheckbox.php

---
+++ userinputfromcheckbox.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset= “utf-8”>
+        <title>
+        </head>
+        <body>
+		
+              <form action= “site.php” method= “post”>
+                    Apples: <input type= “checkbox” name= “fruits[]” value “apples”><br>
+                    Oranges: <input type= “checkbox” name= “fruits[]” value “oranges”><br>
+                    Pears: <input type= “checkbox” name= “fruits[]” value “pears”><br>
+                    <input type=“submit”>
+              </form>
+              <?php
+
+		                $fruits=$_POST[“fruits”];
+                    echo $fruits[0]
+              ?>
+</body>
+</html>
