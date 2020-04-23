12c8a4bee450432e402b37c317a0871a5bb0a84e
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-23 00:03:02 +0000

    Create Variables.php

---
+++ Variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <meta charset=”utf-8”>
+      <title>Variables</title>
+</head>
+<body>
+      <h1>Mike’s site</h1>
+      <?php
+       $characterName = “John”;
+       $characterAge=35;
+       echo “There once was a man named $characterName <br>”;
+       echo “He was $characterAge years old <br>”;
+       echo “He really liked the name $characterName<br>”;
+       echo “But didn’t like being $characterAge<br>”; 
+     ?>
+</body>
+</html>
+
