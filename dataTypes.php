7b004df4f47db1aeb4638132dfff7a5089fc72d1
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-23 00:04:29 +0000

    Create dataTypes.php

---
+++ dataTypes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+  <?php
+      $phrase= "To be or not to be";
+      $age= 30;
+      $gpa=3.91;
+      $isMale=true;
+      $loveSchool=true;
+  ?>
+</body>
+</html>
