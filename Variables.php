62deb759468709fc8b974ac1c888909ef4692c23
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-13 23:00:50 +0100

    Update Variables.php

--- Variables.php
+++ Variables.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-      <meta charset=”utf-8”>
+      <meta charset="utf-8">
       <title>Variables</title>
 </head>
 <body>
-      <h1>Mike’s site</h1>
+      <h1>Omar's site</h1>
       <?php
-       $characterName = “John”;
-       $characterAge=35;
-       echo “There once was a man named $characterName <br>”;
-       echo “He was $characterAge years old <br>”;
-       echo “He really liked the name $characterName<br>”;
-       echo “But didn’t like being $characterAge<br>”; 
+       $characterName = "Ramo";
+       $characterAge=89;
+       echo "Once upon a time an individual named $characterName <br>";
+       echo "was $characterAge years old <br>";
+       echo "He really liked being called $characterName<br>";
+       echo "But hated being $characterAge<br>"; 
      ?>
 </body>
 </html>
